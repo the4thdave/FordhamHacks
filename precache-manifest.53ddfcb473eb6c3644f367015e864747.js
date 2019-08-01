721f7e283f6a8fbc07bc2a4081f997c25dbd8c3e
the4thdave <7dmunoz@gmail.com>
2019-08-01 16:13:10 -0700

    Updates

---
+++ precache-manifest.53ddfcb473eb6c3644f367015e864747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db36c974c189f3b5e9b24ab4cd83c63b",
+    "url": "/FordhamHacks/index.html"
+  },
+  {
+    "revision": "59984f235626aa741cc1",
+    "url": "/FordhamHacks/static/css/main.39c428ce.chunk.css"
+  },
+  {
+    "revision": "d18c1e9834b647a4c4c5",
+    "url": "/FordhamHacks/static/js/2.4d1294ff.chunk.js"
+  },
+  {
+    "revision": "59984f235626aa741cc1",
+    "url": "/FordhamHacks/static/js/main.ccfa557d.chunk.js"
+  },
+  {
+    "revision": "1c2b704897aff003e939",
+    "url": "/FordhamHacks/static/js/runtime~main.443f5b4c.js"
+  }
+]);
