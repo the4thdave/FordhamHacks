486ccdea6cfe5bb09deace9da3b22f1c770c4fbb
the4thdave <7dmunoz@gmail.com>
2019-08-01 16:28:11 -0700

    Update files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,9 +26,6 @@ const Navbar = () => {
                     </a>
                 </li>
             </ul>
-            <a id="mlh-trust-badge" href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=gray">
-                <img id="mlh" src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-gray.svg" alt="Major League Hacking 2020 Hackathon Season"/>
-            </a>
         </div>
     );
 };
