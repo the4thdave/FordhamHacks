32ef3aab401dea7c18cc80c7e7bf962fc9ffd4f4
Helen Dempsey <40031593+mintkey@users.noreply.github.com>
2019-08-08 10:25:54 -0400

    Updated colors and added button

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,9 @@ import cityscape from '../assets/fordhamhackscity.svg'
 const Intro = (props) => {
     return (
         <div className="intro">
-            <h1 className="neon date">January 25-26, 2020</h1>
-            <h1 className="neon location">Fordham University Lincoln Center</h1>
-            <a href="" class="button">
+            <h2 className="neon date">January 25-26, 2020</h2>
+            <h2 className="neon location">Fordham University Lincoln Center</h2>
+            <a href="" class="button1">
               <svg width="275" height="50">
                 <defs>
                   <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
@@ -18,6 +18,18 @@ const Intro = (props) => {
                 <rect x="2" y="5" rx="10" fill="none" stroke="url(#gradient)" width="260" height="40"></rect>
               </svg>
               <span>Pre-Register</span>
+            </a>
+            <a href="" class="button2">
+              <svg width="275" height="50">
+                <defs>
+                  <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                    <stop offset="0%" stop-color="#E178ED"/>
+                    <stop offset="100%" stop-color="#9B7DE1" />
+                  </linearGradient>
+                </defs>
+                <rect x="2" y="5" rx="10" fill="none" stroke="url(#gradient)" width="260" height="40"></rect>
+              </svg>
+              <span>Sponsors</span>
             </a>
             <img id="cityscape" alt="Cityscape" src={cityscape}/>
         </div>
